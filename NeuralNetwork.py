848cfea9e83323b8351b4ed58b0648c9f302289a
Eleanor-Tarring <60779119+Eleanor-Tarring@users.noreply.github.com>
2022-02-24 13:58:44 +0000

    changing layer shapes to allow network to run

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,9 +3,28 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import tensorflow
 import pandas as pd
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
 
-matches = pd.read_csv('allmatches.csv')
-features_list = matches.columns
+from numpy.random import seed
+from tensorflow import random
+seed(0)
+random.set_seed(0)
+import os
+os.environ['TF_DETERMINISTIC_OPS'] = '1'
+
+matches = pd.read_csv('diff_features.csv')
+x = matches.Winner #outcomes of matches in the data
+label_encoder = LabelEncoder()
+x = label_encoder.fit_transform(x)
+x=to_categorical(x)
+
+features_list = [ 'WRank', 'LRank', 'WPts', 'LPts', 'W1', 'L1', 'W2', 'L2', 'W3', 'L3', 'W4', 'L4', 'W5', 'L5', 'Wsets', 'Lsets',] #change this - don't use all columns
+y = matches[features_list]
+
+num_classes = len(np.unique(y))
+num_features = len(features_list)
 
 #Split into training and test data first
 results = matches['Winner'] #outcomes of matches in the data
@@ -14,11 +33,11 @@ num_features = len(features)
 num_classes = np.unique(results)
 print(np.shape(matches))
 
-features_train, features_test, results_train, results_test = train_test_split(features, results, test_size = 0.15) #can add random_state = int to shuffle the data
+y_train, y_test, x_train, x_test = train_test_split(y, x, test_size = 0.15) #can add random_state = int to shuffle the data #can add random_state = int to shuffle the data
 
 scaler = sklearn.preprocessing.StandardScaler()
-features_train = scaler.fit_transform(features_train)
-features_test = scaler.transform(features_test)
+y_train = scaler.fit_transform(y_train)
+y_test = scaler.transform(y_test)
 
 #Use 'relu' or 'sigmoid' for activation
 #Use 'softmax' or 'sigmoid' for output layer
@@ -28,7 +47,7 @@ model.add(tensorflow.keras.layers.Dense(32,input_dim=num_features,activation = '
 model.add(tensorflow.keras.layers.Dense(16,activation='relu'))
 model.add(tensorflow.keras.layers.Dense(8,activation='relu'))
 model.add(tensorflow.keras.layers.Dense(4,activation='relu'))
-model.add(tensorflow.keras.layers.Dense(num_classes,activation='softmax'))
+model.add(tensorflow.keras.layers.Dense(591,activation='softmax'))
 
 #calculate cross-entropy loss
 model.compile(loss='categorical_crossentropy', optimizer=tensorflow.keras.optimisers.SGD(learning_rate=0.01), metrics=['accuracy'])
