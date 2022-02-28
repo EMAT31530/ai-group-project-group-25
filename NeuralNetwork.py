da50b166226c809d2ca0c2411a0167559f536542
Eleanor-Tarring <60779119+Eleanor-Tarring@users.noreply.github.com>
2022-02-28 22:35:22 +0000

    alter code such that as the neural network updates, the best model is saved

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,6 @@
 #import relevant modules
 from sklearn.model_selection import train_test_split
+import sklearn
 import numpy as np
 import tensorflow
 import pandas as pd
@@ -27,13 +28,7 @@ num_classes = len(np.unique(y))
 num_features = len(features_list)
 
 #Split into training and test data first
-results = matches['Winner'] #outcomes of matches in the data
-features = features_list #column headings list
-num_features = len(features)
-num_classes = np.unique(results)
-print(np.shape(matches))
-
-y_train, y_test, x_train, x_test = train_test_split(y, x, test_size = 0.15) #can add random_state = int to shuffle the data #can add random_state = int to shuffle the data
+y_train, y_test, x_train_categorical, x_test_categorical = train_test_split(y, x, test_size = 0.15) #can add random_state = int to shuffle the data #can add random_state = int to shuffle the data
 
 scaler = sklearn.preprocessing.StandardScaler()
 y_train = scaler.fit_transform(y_train)
@@ -52,4 +47,29 @@ model.add(tensorflow.keras.layers.Dense(591,activation='softmax'))
 #calculate cross-entropy loss
 model.compile(loss='categorical_crossentropy', optimizer=tensorflow.keras.optimisers.SGD(learning_rate=0.01), metrics=['accuracy'])
 
+model.summary()
+
 history = model.fit(features_train,results_train, epochs=500)
+
+#review results
+train_acc = model.evaluate(y_train, x_train_categorical, verbose=0)
+test_acc = model.evaluate(y_test, x_test_categorical, verbose=0)
+print(train_acc)
+print(test_acc)
+
+model = tensorflow.keras.models.Sequential()
+model.add(tensorflow.keras.layers.Dense(32,input_dim=num_features,activation = 'relu'))
+model.add(tensorflow.keras.layers.Dense(16,activation='relu'))
+model.add(tensorflow.keras.layers.Dense(8,activation='relu'))
+model.add(tensorflow.keras.layers.Dense(4,activation='relu'))
+model.add(tensorflow.keras.layers.Dense(591,activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=SGD(learning_rate=0.01), metrics=['accuracy'])
+
+checkpoint_path="weights.best.hdf5"
+checkpoint = tensorflow.keras.callbacks.ModelCheckpoint(checkpoint_path,monitor='val accuracy', verbose=0, save_best_only=True, mode = 'max')
+
+callbacks_list = [checkpoint]
+
+# Train the model with the new callback
+model.fit(y_train, x_train_categorical,epochs=100, callbacks=[callbacks_list],verbose=0)
