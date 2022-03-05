ae9659fee93d213fd4ffb9b17623469809aafb71
Eleanor-Tarring <60779119+Eleanor-Tarring@users.noreply.github.com>
2022-03-05 09:36:35 +0000

    Fixed model to calculate loss and alter train and tests sets to increase accuracy

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,6 +7,7 @@ import pandas as pd
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
+from keras.models import load_model
 
 from numpy.random import seed
 from tensorflow import random
@@ -16,19 +17,20 @@ import os
 os.environ['TF_DETERMINISTIC_OPS'] = '1'
 
 matches = pd.read_csv('diff_features.csv')
-x = matches.Winner #outcomes of matches in the data
+matches.dropna()
+x = matches.outcome #outcomes of matches in the data
 label_encoder = LabelEncoder()
 x = label_encoder.fit_transform(x)
 x=to_categorical(x)
 
-features_list = [ 'WRank', 'LRank', 'WPts', 'LPts', 'W1', 'L1', 'W2', 'L2', 'W3', 'L3', 'W4', 'L4', 'W5', 'L5', 'Wsets', 'Lsets',] #change this - don't use all columns
+features_list = ['diff_rank','diff_match_win_percentage','diff_match_win_percentage_hh','diff_match_win_percentage_sets','diff_match_win_percentage_surface','diff_match_win_percentage_surface_sets','diff_game_win_percentage','diff_game_win_percentage_hh','diff_game_win_percentage_sets','diff_game_win_percentage_surface','diff_game_win_percentage_surface_sets']
 y = matches[features_list]
 
 num_classes = len(np.unique(y))
 num_features = len(features_list)
 
 #Split into training and test data first
-y_train, y_test, x_train_categorical, x_test_categorical = train_test_split(y, x, test_size = 0.15) #can add random_state = int to shuffle the data #can add random_state = int to shuffle the data
+y_train, y_test, x_train_categorical, x_test_categorical = train_test_split(y, x, test_size = 0.2) #can add random_state = int to shuffle the data #can add random_state = int to shuffle the data
 
 scaler = sklearn.preprocessing.StandardScaler()
 y_train = scaler.fit_transform(y_train)
@@ -45,11 +47,11 @@ model.add(tensorflow.keras.layers.Dense(4,activation='relu'))
 model.add(tensorflow.keras.layers.Dense(591,activation='softmax'))
 
 #calculate cross-entropy loss
-model.compile(loss='categorical_crossentropy', optimizer=tensorflow.keras.optimisers.SGD(learning_rate=0.01), metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=tensorflow.keras.optimizers.SGD(learning_rate=0.01), metrics=['accuracy'])
 
 model.summary()
 
-history = model.fit(features_train,results_train, epochs=500)
+history = model.fit(y_train,x_train_categorical, epochs=500)
 
 #review results
 train_acc = model.evaluate(y_train, x_train_categorical, verbose=0)
@@ -57,19 +59,19 @@ test_acc = model.evaluate(y_test, x_test_categorical, verbose=0)
 print(train_acc)
 print(test_acc)
 
-model = tensorflow.keras.models.Sequential()
-model.add(tensorflow.keras.layers.Dense(32,input_dim=num_features,activation = 'relu'))
-model.add(tensorflow.keras.layers.Dense(16,activation='relu'))
-model.add(tensorflow.keras.layers.Dense(8,activation='relu'))
-model.add(tensorflow.keras.layers.Dense(4,activation='relu'))
-model.add(tensorflow.keras.layers.Dense(591,activation='softmax'))
+#model = tensorflow.keras.models.Sequential()
+#model.add(tensorflow.keras.layers.Dense(32,input_dim=num_features,activation = 'relu'))
+#model.add(tensorflow.keras.layers.Dense(16,activation='relu'))
+#model.add(tensorflow.keras.layers.Dense(8,activation='relu'))
+#model.add(tensorflow.keras.layers.Dense(4,activation='relu'))
+#model.add(tensorflow.keras.layers.Dense(591,activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer=SGD(learning_rate=0.01), metrics=['accuracy'])
+#model.compile(loss='categorical_crossentropy', optimizer=SGD(learning_rate=0.01), metrics=['accuracy'])
 
-checkpoint_path="weights.best.hdf5"
-checkpoint = tensorflow.keras.callbacks.ModelCheckpoint(checkpoint_path,monitor='val accuracy', verbose=0, save_best_only=True, mode = 'max')
+#checkpoint_path="weights.best.hdf5"
+#checkpoint = tensorflow.keras.callbacks.ModelCheckpoint(checkpoint_path,monitor='val accuracy', verbose=0, save_best_only=True, mode = 'max')
 
-callbacks_list = [checkpoint]
+#callbacks_list = [checkpoint]
 
 # Train the model with the new callback
-model.fit(y_train, x_train_categorical,epochs=100, callbacks=[callbacks_list],verbose=0)
+#model.fit(y_train, x_train_categorical,epochs=100, callbacks=[callbacks_list],verbose=0)
