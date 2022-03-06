7da279e95d6ea478d0d210225f0eed8366736d67
Eleanor-Tarring <60779119+Eleanor-Tarring@users.noreply.github.com>
2022-03-06 21:54:15 +0000

    Create predictions for the 2022 Australian open matches

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -16,21 +16,22 @@ random.set_seed(0)
 import os
 os.environ['TF_DETERMINISTIC_OPS'] = '1'
 
-matches = pd.read_csv('diff_features.csv')
-matches.dropna()
+matches = pd.read_csv('aus_diff_features.csv')
+matches = matches.iloc[0:28960,:]
+matches = matches.dropna()
 x = matches.outcome #outcomes of matches in the data
 label_encoder = LabelEncoder()
 x = label_encoder.fit_transform(x)
-x=to_categorical(x)
+x = to_categorical(x)
 
 features_list = ['diff_rank','diff_match_win_percentage','diff_match_win_percentage_hh','diff_match_win_percentage_sets','diff_match_win_percentage_surface','diff_match_win_percentage_surface_sets','diff_game_win_percentage','diff_game_win_percentage_hh','diff_game_win_percentage_sets','diff_game_win_percentage_surface','diff_game_win_percentage_surface_sets']
 y = matches[features_list]
 
-num_classes = len(np.unique(y))
+num_classes = len(np.unique(x))
 num_features = len(features_list)
 
 #Split into training and test data first
-y_train, y_test, x_train_categorical, x_test_categorical = train_test_split(y, x, test_size = 0.2) #can add random_state = int to shuffle the data #can add random_state = int to shuffle the data
+y_train, y_test, x_train_categorical, x_test_categorical = train_test_split(y, x, test_size = 0.2) #can add random_state = int to shuffle the data
 
 scaler = sklearn.preprocessing.StandardScaler()
 y_train = scaler.fit_transform(y_train)
@@ -40,24 +41,22 @@ y_test = scaler.transform(y_test)
 #Use 'softmax' or 'sigmoid' for output layer
 #Initialise neural network
 model = tensorflow.keras.models.Sequential()
-model.add(tensorflow.keras.layers.Dense(32,input_dim=num_features,activation = 'relu'))
+model.add(tensorflow.keras.layers.Dense(32,input_dim = num_features, activation = 'relu'))
 model.add(tensorflow.keras.layers.Dense(16,activation='relu'))
 model.add(tensorflow.keras.layers.Dense(8,activation='relu'))
 model.add(tensorflow.keras.layers.Dense(4,activation='relu'))
-model.add(tensorflow.keras.layers.Dense(num_classes,activation='softmax'))
+model.add(tensorflow.keras.layers.Dense(2,activation='softmax'))
 
 #calculate cross-entropy loss
-model.compile(loss='categorical_crossentropy', optimizer=tensorflow.keras.optimizers.SGD(learning_rate=0.01), metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 model.summary()
 
-checkpoint_path="weights.{epoch:02d}-{val_loss:.2f}.h5"
-modelcheckpoint = tensorflow.keras.callbacks.ModelCheckpoint(checkpoint_path,monitor='val_accuracy', save_best_only=True, mode = 'max')
-
-callbacks_list = [modelcheckpoint]
+#checkpoint_path="weights.{epoch:02d}-{val_loss:.2f}.h5"
+modelcheckpoint = tensorflow.keras.callbacks.ModelCheckpoint('best_model.h5', monitor='val_accuracy', save_best_only=True, mode = 'max')
 
 # Train the model with the new callback
-history=model.fit(y_train, x_train_categorical, batch_size=128, validation_data=(y_test,x_test_categorical),epochs=100,callbacks=[modelcheckpoint], validation_split=0.1)
+history=model.fit(y_train, x_train_categorical, batch_size=128, validation_data=(y_test,x_test_categorical),epochs=1000,callbacks=[modelcheckpoint], validation_split=0.1)
 
 # evaluate the model
 scores = model.evaluate(y_train, x_train_categorical, verbose=0)
@@ -70,14 +69,31 @@ print(train_acc)
 print(test_acc)
 
 # save model and architecture to single file
-model.save("model.h5")
+#model.save("model.h5")
 
-best_model = load_model("model.h5")
+best_model = load_model('best_model.h5')
 
 #for predictions
 #put matches throught create_features call it ausopen22.csv (for example)
-ausopen22 = pd.read_csv('ausopen.csv')
-new_matches = ausopen22[features_list]
+new_matches = pd.read_csv('aus_diff_features.csv')
+ausopen22 = new_matches.iloc[28960:29024,:]
+print(ausopen22)
+aus_matches = ausopen22[features_list]
 
 #add new column for predictions
-new_matches['predictions'] = best_model.predict_classes(new_matches)
+predictions = best_model.predict(aus_matches)
+actual_pred = []
+for pred in predictions:
+    preds = np.argmax(pred)
+    actual_pred.append(preds)
+
+ausopen22 = ausopen22.assign(predictions = actual_pred)
+
+#add new column for probabilities
+probabilities = best_model.predict(aus_matches)
+df = pd.DataFrame(probabilities, columns=["outcome 0 probability","outcome 1 probability"])
+
+aus22_output = ausopen22[['player_0','player_1','predictions']]
+
+finaldf = pd.concat([aus22_output,df],axis = 1)
+print(finaldf)
